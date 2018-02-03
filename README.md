0dc5dc62b3f4ca2bc91d8d3f50d40b572d05d01c
Bar Admoni <bar6020677@gmail.com>
2018-02-04 01:10:18 +0200

    Init commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# simple-rails-runner
-Basic Rails runner can (and should) run in the background
+# Simple Rails Runner
+Basic Rails runner can (and should) run in the background I created at home for personal project. Enjoy!
+
+## Table of Contents
+* [Requirements](#requirements)
+* [Install](#install)
+* [How To Run](#how-to-run)
+* [Arguments](#arguments)
+
+## Requirements
+* Rails (2.1 or above)
+
+## Install
+Just clone the git or download the file :)
+```bash
+$ git clone git@github.com:baradm100/simple-rails-runner.git
+```
+
+## How To Run
+Just run using Rails runner
+```bash
+# as a daemon (only in bash)
+$ bundle exec rails runner runner.rb &
+# With args:
+$ bundle exec rails runner runner.rb --eval "puts 'hi'" --timeout 0.1
+```
+
+## Arguments
+| Key               | Explanation                                               |
+| ----------------- | --------------------------------------------------------- |
+| --timeout <VALUE> | Set custom timeout for the loop, defualt is 2 second      |
+| --eval <VALUE>    | Running the value as code (using eval, please be careful) |
---
+++ runner.rb
@@ -0,0 +1,62 @@
+TIMEOUT = 2.0.freeze
+PID_PATH = './tmp/pids/runner.pid'.freeze
+
+
+# Parse custom_timeout argument flag
+# @return [nilClass|Integer] what the custom timeout is
+def parse_custom_timeout
+    if ARGV.include?('--timeout')
+        custom_timeout = ARGV[ARGV.index('--timeout') + 1]
+
+        if custom_timeout && custom_timeout.to_f > 0
+            custom_timeout.to_f
+        else
+            nil
+        end
+    end
+end
+
+# Parse custom_eval argument flag
+# @return [nilClass|String] what the custom eval is
+def parse_custom_eval
+    if ARGV.include?('--eval')
+        ARGV[ARGV.index('--eval') + 1]
+    end
+end
+
+# Initializing
+puts 'Starting the runner...'
+
+# if there is a runner that already running then exiting
+if File.exist?(PID_PATH)
+    puts 'There is a runner already running!'
+    puts "Check for pid in: #{PID_PATH}"
+    exit
+end
+
+# creating the full path of the pid folder
+full_path = PID_PATH.split('/').reverse.drop(1).reverse.join('/')
+FileUtils.mkdir_p(full_path)
+
+# writing the runner pid to file
+open(PID_PATH, 'w') { |f| f.puts Process.pid }
+
+# adds +at_exit+, removing PID file if exists
+at_exit do
+    File.delete(PID_PATH) if File.exist?(PID_PATH)
+end
+
+# parsing args
+custom_timeout = parse_custom_timeout()
+custom_eval = parse_custom_eval()
+
+# Infinte loop
+loop do
+    if custom_eval
+        eval custom_eval
+    else
+        puts 'Good morinning!'
+    end
+
+    sleep(custom_timeout || TIMEOUT)
+end
